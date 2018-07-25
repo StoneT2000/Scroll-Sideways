586d37737f6849beba6ed2570c0b02a0d16974d4
Storms2000 <stoneztao@gmail.com>
2018-07-25 19:21:51 -0400

    Initial commit

---
+++ scripts/animation.js
@@ -0,0 +1,123 @@
+var cWidth = window.innerWidth;
+var cHeight = window.innerHeight;
+var count = 0;
+var ids = [];
+var maxParticles = 100;
+var smokeTimer;
+var colorC = false;
+var waterHeight = 150;
+$(document).on("ready", function(){
+  var test = 0;
+  repeatSmoke(50);
+  //The above tiemout must be such that at any one time, max Particles is 10
+  $(".cup").bind("touchstart mousedown", function(e){
+    var offsetx = ($(".container").height()/2 - 4);
+    var posx = e.pageY - offsetx + 95;
+    var percent = 200- posx;
+    console.log(offsetx,posx);
+    if (percent < 10){
+      percent = 10;
+    }
+    else if (percent >= 180){
+      percent = 180;
+    }
+    setWaterHeight(percent);
+  });
+});
+
+function createSmoke(offx,offy){
+
+  var ox = 0;
+  var oy = 0;
+  if (offx){
+    ox = offx;
+  }
+  if (offy){
+    oy = offy;
+  }
+  var thiscount = count;
+  count ++;
+  var randomx = Math.round (Math.random(0,1) * 400) - 200;
+  //randomx = 0;
+  //ox = randomx;
+  var randomAngle = Math.round(Math.random(0,1)*600 -300);
+  //randomAngle = 0;
+  $("#waterglass").append("<div class=\"smoke\" id=\"smoke" + thiscount + "\"></div>")
+  $("#smoke" + thiscount).css("transform","translate(" + ox +"px," + oy +"px) rotate(0deg)");
+  ids.push(thiscount);
+  window.setTimeout(function(){
+    var smokeE = $("#smoke" + ids[ids.length-1]);
+    smokeE.css("transform","translate(" + randomx +"px,-300px) rotate(" + randomAngle + "deg)");
+    smokeE.css("opacity","0");
+    var rand = Math.random(0,1)*20 + 20
+    smokeE.css("width", rand +"px");
+    smokeE.css("height",rand +"px");
+    
+  }, 100);
+  window.setTimeout(function(){
+    $("#smoke" + ids[0]).remove();
+
+    ids.shift();
+    if (count > maxParticles){
+      count = 0;
+    }
+  }, 4000);
+  
+}
+function changeWaterColor(arg){
+  if (arg == 0){
+    $("#water").css("background-color","deepskyblue");
+    $("#watert").css("border","2px dashed");
+    $("#watert").css("color","deepskyblue");
+    $("#coffeet").css("border","none");
+    $("#coffeet").css("color","inherit");
+  }
+  else if (arg == 1){
+    $("#water").css("background-color","rgba(222, 184, 135, 1)"); 
+    $("#watert").css("border","none");
+    $("#watert").css("color","inherit");
+    $("#coffeet").css("border","2px dashed");
+    $("#coffeet").css("color","rgba(222, 184, 135, 1)");
+  }
+}
+function toggleFullScreen() {
+  if (!document.fullscreenElement &&    // alternative standard method
+      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
+
+function repeatSmoke(offy){
+  var timeOut = Math.random(0,1)*400 + 150;
+  smokeTimer = window.setTimeout(function(){
+    createSmoke(-20, offy);
+    repeatSmoke(offy);
+  }, timeOut)
+}
+
+function setWaterHeight(height){
+  var displace = 190 - height;
+  //change css for water and start pos of smoke
+  $("#water").css("height",height+"px");
+  $("#water").css("transform","translate(-50%," + displace + "px)");
+  clearTimeout(smokeTimer);
+  repeatSmoke(displace+10);
+}
---
+++ scripts/scroll.js
@@ -0,0 +1,168 @@
+var p0_timers = [];
+$(document).on("ready",function(){
+  var halfW = window.innerWidth;
+  halfW/=2
+  halfW-=150;
+  $("#p0").css("left",halfW + "px");
+  
+  var last_known_scroll_position = 0;
+  var prev_scroll_pos = 0;
+  var ticking = false;
+  var timer = 0;
+  
+  var left_break_stage = 0;
+  function doSomething(scroll_pos) {
+    // do something with the scroll position
+    if (scroll_pos >= halfW+180){
+      //$("#p0 p").css("display","block");
+      for (var i = 0; i< p0_timers.length; i++){
+        console.log("clear!")
+        clearInterval(p0_timers[i]);
+        if (i == p0_timers.length-1){
+          p0_timers = [];
+        }
+      }
+    } 
+    console.log(scroll_pos)
+    if (left_break_stage >= 0){
+      if (scroll_pos + 50 <= prev_scroll_pos && scroll_pos == 0){
+        //Scrolling left...
+        console.log("hit left side")
+        if (left_break_stage == 0){
+          $(".container").css("margin-left","50px");
+          //$("body").css("width","43650px");
+          left_break_stage ++;
+        }
+        else if (left_break_stage == 1){
+          left_break_stage ++;
+          $(".container").css("margin-left","100px");
+        }
+        else if (left_break_stage == 2){
+          $(".container").css("margin-left","5200px");
+          //Break left wall
+          left_break_stage ++;
+        }
+        
+      }
+    }
+  }
+
+  //Listen for scrolling
+  window.addEventListener('scroll', function(e) {
+    prev_scroll_pos = last_known_scroll_position;
+    last_known_scroll_position = window.scrollX;
+
+    if (!ticking) {
+
+      window.requestAnimationFrame(function() {
+        doSomething(last_known_scroll_position);
+        ticking = false;
+      });
+
+      ticking = true;
+
+    }
+
+  });
+  
+  
+  
+  //Specific timing
+
+  //store last index0
+  var pindex0 = 0;
+  p0_timers.push(window.setTimeout(function(){
+    if (last_known_scroll_position < halfW+180){
+      $("#p0 p").css("display","block");
+    }
+  },5000));
+  p0_timers.push(window.setTimeout(function(){
+    if (last_known_scroll_position < halfW+180){
+      $("#p0 p").text("(Seriously this site has no meaning if you don't scroll right.)");
+    }
+  },10000));
+  p0_timers.push(window.setTimeout(function(){
+    if (last_known_scroll_position < halfW+180){
+      $("#p0 p").text("(Wow do you just want to see me talk instead of exploring???)");
+    }
+  },15000));
+  p0_timers.push(window.setTimeout(function(){
+    if (last_known_scroll_position < halfW+180){
+      $("#p0 p").text("(So be it then... just click this to one-way talk to me.)");
+    }
+    $("#p0 p").css("cursor","pointer");
+    $("#p0 p").on("click", function(){
+      var index0 = Math.round(Math.random(0,1)*(messages0.length - 1));
+      if (index0 == pindex0){
+        index0++;
+        index0 = index0 % messages0.length
+      }
+      pindex0 = index0;
+      $("#p0 p").text("(" + messages0[index0] +")")
+    });
+  },20000));
+  //$("#rating").on("click", );
+  jQuery('img.svg').each(function(){
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');  
+  });
+})
+
+function parseRating(){
+    var orig = $("#ratingInput").val();
+    var rating = parseFloat($("#ratingInput").val());
+    var resthtml = "<input id=\"ratingInput\" style=\"height:50px;text-align: center;margin-top: 20px;\" type=\"text\" placeholder=\"1-10\"><div id=\"rating\" style=\"background-color:deepskyblue;color:white;margin-top:20px;cursor:pointer;\" onclick=\"parseRating()\">Submit</div>"
+    if (!isNaN(rating)){
+      if (rating <1){
+        $("#ratingFeedback").html("You know what 1-10 means right..." + resthtml);
+      }
+      else if (rating <= 2){
+        $("#ratingFeedback").html("Pretty accurate rating" + resthtml);
+      }
+      else if (rating <= 4){
+        $("#ratingFeedback").html("Huh, a little high don't you think?" + resthtml);
+      }
+      else if (rating <=6){
+        $("#ratingFeedback").html("Wow. That high? Either you like useless stuff or you gave a higher rating to see what I had to say" + resthtml);
+      }
+      else if (rating <=8){
+        $("#ratingFeedback").html("A rating of 1 means extremely pointless, you know that right?" + resthtml)
+      }
+      else if (rating<=10){
+        $("#ratingFeedback").html("So if you gave a site this rating, you are basically saying this site is one of the most useful and meaningful sites in the world? I don't believe you." + resthtml);
+      }
+      else if (rating > 10){
+        $("#ratingFeedback").html("You know what 1-10 means right..." + resthtml);
+      }
+      $("#ratingInput").val(rating);
+    }
+    else {
+      $("#ratingFeedback").html("From 1-10, how pointless is this site? Be honest." + resthtml);
+      $("#ratingInput").val(orig);
+    }
+  
+    
+}
